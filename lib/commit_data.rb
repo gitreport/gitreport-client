53b98ee4b409b1afa61ad0799f85e8e2fa4efb38
Jan Roesner <jan@roesner.it>
2011-09-12 17:51:49 +0200

    CommitData class is in.
    CommitData aggregates data from Commit and Project and provides methods to give back that data as a raw hash or as JSON string.

---
+++ lib/commit_data.rb
@@ -0,0 +1,38 @@
+require 'json'
+module GitAccount
+  class CommitData
+
+    def initialize project
+      @data = {}
+      @project = project
+      @commit = GitAccount::Commit.new(project.log.last)
+      set_data
+    end
+
+    def data
+      @data
+    end
+
+    def to_json
+      data.to_json
+    end
+
+    private
+
+    def set_data
+      @data[:sha]             = @commit.sha
+      @data[:short_sha]       = @commit.short_sha
+      @data[:author_name]     = @commit.author.name
+      @data[:author_email]    = @commit.author.email
+      @data[:time]            = @commit.time.xmlschema
+      @data[:message]         = @commit.message
+      @data[:project_path]    = @project.path
+      @data[:project_name]    = @project.name
+      @data[:current_branch]  = @project.branch.name
+      @data[:remotes]         = @project.remotes.map(&:name)
+      @data[:remote_urls]     = @project.remotes.map(&:url)
+      @data[:remote_branches] = @project.remote_branches
+    end
+  end
+
+end
