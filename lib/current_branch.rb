e0040b439bc71d629ce5fe87d8fcb6129efc90a5
Jan Roesner <jan@roesner.it>
2011-09-19 20:52:33 +0200

    Alternative method for current branch and 1.9.2
    Some projects don't want to return the current branch in the expected way. There is a new method alt_name that returns the name in an alternate way. 
    
    Second the configuration was not compatible to 1.9.2 cause the dynamic attr_acessors were private. I made the generator public so the methods are. Not nice but works.

--- lib/current_branch.rb
+++ lib/current_branch.rb
@@ -10,12 +10,20 @@ module GitAccount
 
     # returns the name of the currently checked out branch
     def name
-      @project.branches.select{|b| b.current}.first.full
+      @project.branches.select{|b| b.current}.first.full rescue alt_name
     end
 
     # return the remote of this branch if it's tracking any, else nil
     def tracking
       g.config.to_a.select{|ary| ary.first.match(/^branch/)} #TODo
+    end
+
+    private
+
+    # alternative method to return the current branches name
+    def alt_name
+      name = `git branch`.split("\n").select{|b| b.match(/^\*/)}.first.match(/^\*(.*)$/)
+      return $1.strip
     end
   end
 end
