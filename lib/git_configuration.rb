40c0d58519c5622233eadb73845b79d5da559da3
Jan Roesner <jan@roesner.it>
2011-10-05 12:35:29 +0200

    1.9.2 compatibility …
    In reality it's been a bug. Accessising a hash should be done properly.

--- lib/git_configuration.rb
+++ lib/git_configuration.rb
@@ -3,12 +3,12 @@ module GitReport
 
     # returns the current users name from git config
     def self.user_name
-      @@username ||= GitReport.project.project.config('user.name')
+      @@username ||= GitReport.project.project.config['user.name']
     end
 
     # returns the current users email from git config
     def self.user_email
-      @@useremail ||= GitReport.project.project.config('user.email')
+      @@useremail ||= GitReport.project.project.config['user.email']
     end
   end
 end
