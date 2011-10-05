078c1c6687fc51868055296072bbd84e00054cf5
Jan Roesner <jan@roesner.it>
2011-10-05 23:45:55 +0200

    Supplier spec finished.

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -11,9 +11,34 @@ describe 'GitReport::Supplier' do
   end
 
   describe '#commits!' do
-    it 'should return last and stored commits if called with :last_and_stored'
-    it 'should return stored commits if called with :stored'
-    it 'should return the whole history if called with history'
+
+    it 'should return last and stored commits if called with :last_and_stored' do
+      GitReport::Supplier.send(:class_variable_set, :@@all_commits, nil)
+      GitReport::Supplier.stub!(:stored_commits).and_return([GitReport::Commit.new(@project.log.commits.first)])
+      commits = GitReport::Supplier.commits(:last_and_stored)
+
+      commits.count.should == 2
+      commits.map(&:sha).include?(@project.log.commits.first.sha)
+      commits.map(&:sha).include?(@project.log.commits.last.sha)
+    end
+
+    it 'should return stored commits if called with :stored' do
+      GitReport::Supplier.stub!(:stored_commits).and_return([GitReport::Commit.new(@project.log.commits.first)])
+      commits = GitReport::Supplier.commits(:stored)
+
+      commits.count.should == 1
+      commits.map(&:sha).include?(@project.log.commits.first.sha)
+    end
+
+    it 'should return the whole history if called with history' do
+      commits = GitReport::Supplier.commits(:history)
+
+      commits.count.should == 3
+      commits.each do |commit|
+        @project.revlist.include?(commit.sha).should be_true
+      end
+    end
+
   end
 
 end
