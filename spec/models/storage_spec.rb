fe44998dec498b54d6e11c8ea93aa45b3b553f4b
Jan Roesner <jan@roesner.it>
2011-11-22 20:07:24 +0100

    Added two tests to prove the sync bug.

--- spec/models/storage_spec.rb
+++ spec/models/storage_spec.rb
@@ -47,6 +47,16 @@ describe 'GitReport::Storage' do
       restore.last.foo.should  == f2.foo
       restore.last.bar.should  == f2.bar
     end
+
+    it 'should return an empty array in case no storage exists yet' do
+      tempfile = nil
+      tempdir = File.dirname(@tempfile.path)
+      storage = GitReport::Storage.new(@tempdir, @tempfile)
+
+      expect{ storage.load }.to_not raise_error
+      storage.load.should == []
+    end
+
   end
 
 end
